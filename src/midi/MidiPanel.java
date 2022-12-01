55902446e2c1dc443a7d94c17d42b6e706e050f3
ajd6735 <117790810+ajd6735@users.noreply.github.com>
2022-12-01 01:41:05 -0500

    git commit

---
+++ src/midi/MidiPanel.java
@@ -0,0 +1,17 @@
+package midi;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MidiPanel extends JPanel {
+
+    private MidiGraphics midi;
+    public MidiPanel(){
+        setLayout(new BorderLayout());
+        midi = new MidiGraphics();
+ //       midi.setPreferredSize(new Dimension(300,300));
+        add(midi);
+    }
+
+
+}
